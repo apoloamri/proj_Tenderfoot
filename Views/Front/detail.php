26e7f9591586de13ceff5d35241109a6b826d91e
Paolo Mari Macaraig <pao.mari.macaraig@gmail.com>
2018-11-28 18:09:22 +0800

    20181128 css update

--- Views/Front/detail.php
+++ Views/Front/detail.php
@@ -1,22 +1,24 @@
 <?php $this->Partial("menu") ?>
 <?php $this->Partial("categories") ?>
 <div id="mainContent">
-    <a href="/">Home</a> ›
-    <?php echo $this->URI[2]; ?>
-    <hr/>
-    <div id="detailsImage" style="background-image: url('<?php echo $this->Result->ImagePaths[0]; ?>')"></div>
-    <div id="detailsContent">
-        <h2>
-            <?php echo $this->Result->str_brand; ?> -
-            <?php echo $this->Result->str_name; ?>
-        </h2>
-        <h1>₱<?php echo $this->Result->dbl_price; ?></h1>
-        <p><?php echo $this->Result->txt_description; ?></p>
-        <?php if ($this->Result->int_amount != null && $this->Result->int_amount != 0) { ?>
-            <button v-on:click="AddCart();">Add to cart</button>
-        <?php } else { ?>
-            <button disabled>Out of stock</button>
-        <?php } ?>
+    <div id="details">
+        <a href="/">Home</a> ›
+        <?php echo $this->URI[2]; ?>
+        <hr/>
+        <div id="detailsImage" style="background-image: url('<?php echo $this->Result->ImagePaths[0]; ?>')"></div>
+        <div id="detailsContent">
+            <h2>
+                <?php echo $this->Result->str_brand; ?> -
+                <?php echo $this->Result->str_name; ?>
+            </h2>
+            <h1>₱<?php echo $this->Result->dbl_price; ?></h1>
+            <p><?php echo $this->Result->txt_description; ?></p>
+            <?php if ($this->Result->int_amount != null && $this->Result->int_amount != 0) { ?>
+                <button v-on:click="AddCart();">Add to cart</button>
+            <?php } else { ?>
+                <button disabled>Out of stock</button>
+            <?php } ?>
+        </div>
     </div>
 </div>
 
